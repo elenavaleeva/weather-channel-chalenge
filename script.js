ae08fd18634e2e1259a4c59de74cff7c406fb84b
ELENA VALEEVA <elenavaleeva05@gmail.com>
2023-01-23 22:51:11 -0500

    final touch

--- script.js
+++ script.js
@@ -1,47 +1,74 @@
 const apiKey = 'a71e1dbc556af89167017b8f90cda21e';
+
+
 const form = document.querySelector('#form');
 const input = document.querySelector('#inputCity');
 const content = document.querySelector('.content');
 
-
+// show history
 renderHistory();
 
+// listen sending forms
 form.onsubmit = submitHandler;
 
-function submitHandler (e) {
+// sending forms
+function submitHandler(e) {
+	// prevent default
 	e.preventDefault();
 
-	content.innerHTML = '';
+	// cutting of spaces
 	let city = input.value.trim();
 
+	// clearing input
 	input.value = '';
 
-    showWeather(city);
+	// returning input focus
+	input.focus();
+
+	if (!city) return;
+
+	// show weather
+	showWeather(city);
 }
 
+// show weather
 async function showWeather(city) {
+	// clear history
+	content.innerHTML = '';
 
+	// save history
 	saveHistory(city);
+
+	// show history
 	renderHistory();
 
+	// getting city data
 	const cityInfo = await getGeo(city); // 51.5073219 -0.1276474
 
+	// getting weather data
 	const weatherInfo = await getWeather(cityInfo.lat, cityInfo.lon);
+
+	// getting current weather
 	const currentWeather = parseCurrentWeather(weatherInfo);
+
+	// showing current weather
 	renderCurrentWeather(cityInfo, currentWeather);
+
+	// finding next days
 	const nextDaysWeather = findNextDaysWeather(weatherInfo);
+
+	// showing next days
 	renderNextDays(nextDaysWeather);
-};
+}
 
-
-async function getGeo (city) {
-	`http://openweathermap.org/api/geocoding-api`
-	const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`;
+async function getGeo(city) {
+	// https://openweathermap.org/api/geocoding-api
+	const geoUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`;
 	const response = await fetch(geoUrl);
 	const data = await response.json();
-    console.log('getGeo', data);
+	console.log('getGeo', data);
 
-    const object = {
+	const object = {
 		name: data[0]['name'],
 		country: data[0]['country'],
 		lat: data[0]['lat'],
@@ -52,28 +79,28 @@ async function getGeo (city) {
 }
 
 async function getWeather(lat, lon) {
-	const url = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+	const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
 	const response = await fetch(url);
 	const data = await response.json();
 	console.log('getWeather', data);
 	return data;
 }
 
-function parseCurrentWeather (data) {
-    const current = data.list[0];
-    console.log('current', current);
+function parseCurrentWeather(data) {
+	const current = data.list[0];
+	console.log('current', current);
 
-    const object = {
-        city: data.city.name,
-        time: current.dt,
-        temp: current.main.temp,
-        desc: current.weather[0]['description'],
-        wind: current.wind.speed,
-        icon: current.weather[0]['icon'],
-    };
+	const object = {
+		city: data.city.name,
+		time: current.dt,
+		temp: current.main.temp,
+		desc: current.weather[0]['description'],
+		wind: current.wind.speed,
+		icon: current.weather[0]['icon'],
+	};
 
-    console.log('object', object);
-    return object;
+	console.log('object', object);
+	return object;
 }
 
 function renderCurrentWeather(cityInfo, currentWeather) {
@@ -90,7 +117,7 @@ function renderCurrentWeather(cityInfo, currentWeather) {
                 <ul class="details">
                     <li class="details__item">${currentWeather.temp}°f</li>
                     <li class="details__item">${currentWeather.desc}</li>
-                    <li class="details__item">wind ${currentWeather.wind} м/с</li>
+                    <li class="details__item"> wind ${currentWeather.wind} m/h</li>
                 </ul>
             </div>`;
 
@@ -98,47 +125,44 @@ function renderCurrentWeather(cityInfo, currentWeather) {
 }
 
 function findNextDaysWeather(findNextDay) {
-    console.log('today', new Date());
-	const tomorrow = new Date();
-	tomorrow.setUTCDate(tomorrow.getDate() + 1);
-    tomorrow.setUTCHours(12, 0, 0, 0)
-    console.log('tomorrow', tomorrow);
+	const days = [];
+	console.log(findNextDay.list[0]['dt']);
 
-    const days = []
+	for (let index = 1; index < 6; index++) {
+		let nextDay = new Date(findNextDay.list[0]['dt'] * 1000);
+		nextDay.setUTCDate(nextDay.getUTCDate() + index);
 
-    for (let index = 1; index < 6; index++){
-        let nextDay = new Date();
-        nextDay.setDate(nextDay.getDate() + index);
-        nextDay.setHours(12, 0, 0, 0);
-        console.log('nextDay', nextDay);
-        days.push(nextDay.getTime());
-    }
+		nextDay.setUTCHours(12, 0, 0, 0);
+		console.log('nextDay', nextDay);
+		console.log('nextDay', nextDay.getUTCHours());
 
-    console.log(days);
+		days.push(nextDay.getTime());
+	}
 
-    const nextDaysWeather = findNextDay.list.filter((item)=>{
+	console.log(days);
 
-        for (const day of days) {
-            if (item.dt * 1000 === day) {
-                return true;
+	const nextDaysWeather = findNextDay.list.filter((item) => {
+		for (const day of days) {
+			if (item.dt * 1000 === day) {
+				return true;
 			}
 		}
-    })
+	});
 
-    console.log('nextDaysWeather', nextDaysWeather);
-    return nextDaysWeather;
+	console.log('nextDaysWeather', nextDaysWeather);
+	return nextDaysWeather;
 }
 
-function renderNextDays (nextDays){
+function renderNextDays(nextDays) {
 	const futureEl = document.createElement('section');
 	futureEl.classList.add('future');
 
 	futureEl.insertAdjacentHTML(
 		'afterbegin',
-		`<h2 class="title-2">Weather Forecast ${nextDays.length} days</h2>`
+		`<h2 class="title-2">Weather Forecast for  ${nextDays.length} days</h2>`
 	);
 
-    const cardsRow = document.createElement('div');
+	const cardsRow = document.createElement('div');
 	cardsRow.classList.add('cards-row');
 
 	for (const day of nextDays) {
@@ -154,53 +178,58 @@ function renderNextDays (nextDays){
                             <li class="details__item">${
 								day.weather[0]['main']
 							}</li>
-                            <li class="details__item">${day.wind.speed} m/c</li>
+                            <li class="details__item">${day.wind.speed} m/s</li>
                         </ul>
                     </section>`;
 
 		cardsRow.insertAdjacentHTML('beforeend', card);
 	}
 
-    futureEl.insertAdjacentElement('beforeend', cardsRow);
-    content.insertAdjacentElement('beforeend', futureEl);
+	futureEl.insertAdjacentElement('beforeend', cardsRow);
+	content.insertAdjacentElement('beforeend', futureEl);
 }
-
 
 function saveHistory(city) {
-    let history = [];
-    if (localStorage.getItem('history')) {
-        history = JSON.parse(localStorage.getItem('history'))
-    }
+	let history = [];
+	if (localStorage.getItem('history')) {
+		history = JSON.parse(localStorage.getItem('history'));
+	}
+// if city exist than putt it up
+	if (history.indexOf(city) !== -1) {
+		const index = history.indexOf(city);
+		const moveCity = history.splice(index, 1);
+		history.unshift(moveCity[0]);
+	}
 
-    if (history.length >= 5) history.pop();
+	// if city not exist - adding forward, deleting last 
+	if (history.indexOf(city) === -1) {
+        if (history.length >= 5) history.pop();
+        history.unshift(city);
+	}
 
-    history.unshift(city);
-
-    localStorage.setItem('history', JSON.stringify(history));
+	localStorage.setItem('history', JSON.stringify(history));
 }
 
-function renderHistory () {
-    const historyWrapper = document.querySelector('.history');
-    historyWrapper.innerHTML = '';
+function renderHistory() {
+	const historyWrapper = document.querySelector('.history');
+	historyWrapper.innerHTML = '';
 
-    let history = [];
+	let history = [];
 	if (localStorage.getItem('history')) {
 		history = JSON.parse(localStorage.getItem('history'));
 	}
 
-    if (history.length === 0) return false
+	if (history.length === 0) return false;
 
+	for (let item of history) {
+		const button = document.createElement('button');
+		button.classList.add('history__item');
+		button.innerText = item;
 
-    for (let item of history) {
-        let button = document.createElement('button');
-        button.classList.add('history__item');
-        button.innerText = item;
+		button.onclick = () => {
+			showWeather(item);
+		};
 
-        button.onclick = () => {
-            showWeather(item);
-        };
-
-    
-        historyWrapper.insertAdjacentElement('beforeend', button);
-    }
+		historyWrapper.insertAdjacentElement('beforeend', button);
+	}
 }
